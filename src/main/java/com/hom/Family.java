aede496622a56c386d68484c1874c741908412aa
huanshan_pro <goodMorning@atguigu.com>
2020-11-21 17:01:40 +0800

    添加文件到本地库

---
+++ src/main/java/com/hom/Family.java
@@ -0,0 +1,32 @@
+package com.hom;
+
+import java.util.Scanner;
+
+public class Family {
+    /*
+     * 求你的身高
+     *
+     */
+    public static void main(String[] args) {
+        int a = 0;
+
+        //从键盘输入数据
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("请输入数据: ");
+        //请输入数据
+        a = Integer.parseInt(scanner.next());
+        switch (a){
+            case 0:
+                System.out.println("我是0");
+                break;
+            case 1:
+                System.out.println("我是1");
+                break;
+            default:
+                System.out.println("您输入的数不符合规则");
+        }
+
+
+
+    }
+}
